c118083c48c15d2fc35d1b71883e038196a41658
dadolyner <skulj.david@gmail.com>
2022-05-31 19:37:34 +0200

    working google maps integration

--- src/pages/LocationGuess.tsx
+++ src/pages/LocationGuess.tsx
@@ -4,29 +4,30 @@ import Footer from '../components/Footer/footer';
 import { Container, MapsContainer, LocationImage, GoogleMapsContainer, GuessInfo, LeaderboardContainer, Input } from '../styles/LocationGuess.styled';
 import { Header4, Paragraph, GreenText } from '../components/Typography/typography.styled';
 import { GreenButton } from '../components/Buttons/buttons.styled';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import Geocode from "react-geocode";
+import { GoogleMap, useLoadScript, Marker,  } from '@react-google-maps/api';
 import { ImagePlaceholder } from '../images/ImageExporter';
 
 const GuessLocation: React.FC = () => {
-    const client = require('@bigdatacloudapi/client')(process.env.REACT_APP_GEO_KEY)
 	const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY });
     const [latitude, setLatitude] = React.useState(0);
     const [longitude, setLongitude] = React.useState(0);
-    const [location, setLocation] = React.useState(Object);
+    const [location, setLocation] = React.useState(null);
+
+    Geocode.setApiKey(process.env.REACT_APP_GOOGLE_KEY);
+    Geocode.setLanguage("en");
 
     const center = React.useMemo(() => ({ lat: 46, lng: 14 }), []);
-
     const handleChangeCoords = async (event: any) => {
         try {
             setLatitude(event.latLng.lat());
-            setLongitude(event.latLng.lng());
-            
-            const stringLocation = await client.getReverseGeocode({
-                latitude: event.latLng.lat(),
-                longitude: event.latLng.lng()
-            })
-            setLocation(stringLocation)
-        } catch (error) { }
+            setLongitude(event.latLng.lng()); 
+
+            Geocode.fromLatLng(event.latLng.lat(), event.latLng.lng()).then(
+                (response) =>  setLocation(response.results[0].formatted_address),
+                (error) => console.error(error)
+            );
+        } catch (error) {}
     }
 
 	if (isLoaded) {
@@ -40,10 +41,8 @@ const GuessLocation: React.FC = () => {
 							Take a <GreenText>guess</GreenText>!
 						</Header4>
 
-						<LocationImage>
-                            <img src={ImagePlaceholder} alt={'location_image.png'} height={'300px'} width={'100%'} />
-                        </LocationImage>
-                        <br/>
+						<LocationImage><img src={ImagePlaceholder} alt={'location_image.png'} height={'300px'} width={'100%'} /></LocationImage>
+
 						<GoogleMapsContainer>
 							<GoogleMap
                                 zoom={10} 
@@ -51,23 +50,21 @@ const GuessLocation: React.FC = () => {
                                 mapContainerStyle={{ height: '300px', width: '100%' }} 
                                 onClick={(event: any) => handleChangeCoords(event)} >
                                     
-                                <Marker position={{ lat: latitude, lng: longitude }} />
+                                <Marker key={new Date().getTime().toString()} position={{ lat: latitude , lng: longitude }} />
                             </GoogleMap>
-						</GoogleMapsContainer>
+						</GoogleMapsContainer><br/>
 
 						<GuessInfo>
-							<Paragraph style={{ textAlign: 'left' }}>Error distance</Paragraph>
-							<div></div>
+							<Paragraph style={{ textAlign: 'left' }}>Error distance</Paragraph><br/>
 							<Paragraph style={{ textAlign: 'left' }}>Guessed location</Paragraph>
 
-							<Input type='text' placeholder='0.5' disabled />
-							<div></div>
-							<Input type='text' value={location.city ? location.city : location.principalSubdivision} placeholder='City' disabled/>
+							<Input type='text' placeholder='0' disabled /><br/>
+							<Input type='text' value={location ? location : ''} placeholder='City' disabled/>
 						</GuessInfo>
 						<GreenButton style={{ width: '150px', float: 'right' }}>GUESS</GreenButton>
 					</MapsContainer>
 
-					<div></div>
+					<br/>
 
 					<LeaderboardContainer>
 						<Header4 style={{ textAlign: 'left' }}>Leaderboard</Header4>
