0938092d04c2b839ef71512b000decda625101e2
dadolyner <skulj.david@gmail.com>
2022-06-03 12:30:29 +0200

    adding api ip to env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:3000'
+    baseURL: process.env.REACT_APP_SERVER_IP,
 })
 
 export default instance;
